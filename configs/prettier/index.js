425467090e13dd7f183c08e000f210bb501b84d4
김도현 <swputer@gmail.com>
2025-05-01 22:37:21 +0900

    refactor: Prettier 설정 정리

---
+++ configs/prettier/index.js
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const defaults = {
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  proseWrap: 'preserve',
+  tailwindFunctions: ['clsx'],
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
+};
+
+module.exports = defaults;
