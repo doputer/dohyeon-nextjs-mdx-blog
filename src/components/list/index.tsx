d818937789ae681a1451ebde32da20c116925146
김도현 <swputer@gmail.com>
2025-06-21 12:27:36 +0900

    List 컴포넌트 UI (#63)

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -8,18 +8,18 @@ interface ListProps {
 
 const List = ({ posts }: ListProps) => {
   return (
-    <div className="divide-line divide-y divide-dashed">
+    <div className="grid grid-flow-row auto-rows-fr">
       {posts.map(({ frontmatter, slug }) => (
         <Link
           key={frontmatter.title}
           href={`/${slug}`}
-          className="group max-mobile:px-3 max-mobile:not-first-of-type:pt-4 max-mobile:not-last-of-type:pb-4 max-mobile:gap-6 flex items-center gap-8 px-6 not-first-of-type:pt-8 not-last-of-type:pb-8"
+          className="group max-mobile:py-4 max-mobile:gap-6 flex items-center gap-8 py-6"
         >
           <div className="group-hover:animate-flip max-mobile:text-4xl pointer-events-none text-5xl select-none">
             {frontmatter.emoji}
           </div>
           <div className="flex-1">
-            <div className="group-hover:text-secondary max-mobile:text-base block text-xl font-medium break-keep transition-colors duration-300 ease-out">
+            <div className="group-hover:text-secondary max-mobile:text-base text-xl font-medium transition-colors duration-300 ease-out">
               {frontmatter.title}
             </div>
             <div className="text-muted max-mobile:text-sm max-mobile:mb-2 max-mobile:mt-1 my-2">
