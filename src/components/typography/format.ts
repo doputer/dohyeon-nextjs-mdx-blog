9009f4b6864766ba793b9654b4e21b44ef42fb39
김도현 <swputer@gmail.com>
2025-06-28 00:34:35 +0900

    feat: 텍스트 가독성

--- src/components/typography/format.ts
+++ src/components/typography/format.ts
@@ -7,7 +7,7 @@ const LIST = cn('my-4 list-inside leading-8 [&_ol]:my-0 [&_ol]:ml-4 [&_ul]:my-0 
 // Element
 export const h2 = cn(HEADING, 'text-2xl');
 export const h3 = cn(HEADING, 'text-xl');
-export const blockquote = cn('bg-surface my-4 space-y-4 rounded-lg p-4 [&>p]:m-0');
+export const blockquote = cn('bg-surface my-4 flex flex-col gap-4 rounded-lg p-4 *:m-0');
 export const code = cn('text-sm text-black dark:text-white');
 export const ol = cn(LIST, 'list-decimal');
 export const ul = cn(LIST, 'list-disc');
@@ -17,4 +17,4 @@ export const th = cn('p-2');
 export const td = cn('p-2');
 export const a = cn('text-link underline underline-offset-4');
 export const strong = cn('font-medium text-black dark:text-white');
-export const img = cn('m-auto my-4 h-auto w-fit rounded-sm');
+export const img = cn('m-auto my-4 aspect-auto h-auto w-full max-w-1/2 rounded-sm');
