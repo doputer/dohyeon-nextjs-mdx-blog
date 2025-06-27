64ceb9f20481853847c884bc85dd296f1ebd8372
김도현 <swputer@gmail.com>
2025-06-28 00:35:26 +0900

    feat: 불필요한 코드 제거

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -2,22 +2,13 @@ import { useCallback } from 'react';
 
 interface ScrollOption {
   id?: string;
-  page?: 'top' | 'bottom' | false;
 }
 
 const useScroll = () => {
-  return useCallback(({ id, page = false }: ScrollOption) => {
-    if (page === 'top') {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-      return;
-    } else if (page === 'bottom') {
-      window.scrollTo({ top: window.document.documentElement.scrollHeight, behavior: 'smooth' });
-      return;
-    }
-
+  return useCallback(({ id }: ScrollOption) => {
     if (!id) return;
 
-    const element = document.querySelector<HTMLHeadingElement>('#' + id);
+    const element = document.getElementById(id);
 
     if (!(element instanceof HTMLHeadingElement)) return;
 
