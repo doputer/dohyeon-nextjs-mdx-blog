6b4aa59563841182391d90be830e3b1014848f4a
김도현 <swputer@gmail.com>
2025-06-28 22:20:23 +0900

    타이포그래피 개선 (#69)

--- src/components/toc/index.tsx
+++ src/components/toc/index.tsx
@@ -15,23 +15,22 @@ const TOC = ({ toc }: TOCProps) => {
 
   return (
     <aside className="static left-full h-full xl:absolute">
-      <div className="border-line sticky top-32 w-fit xl:ml-8">
-        <ul className="flex list-inside flex-col gap-2 xl:text-nowrap">
-          {toc.map(({ id, text, depth }) => (
-            <li
-              key={id}
+      <ul className="sticky top-32 flex w-fit list-inside flex-col gap-2 xl:ml-8 xl:text-nowrap">
+        {toc.map(({ id, text, depth }) => (
+          <li key={id} className="list-disc xl:list-none">
+            <button
               className={cn(
-                'xl:text-subtle cursor-pointer list-disc xl:list-none',
-                id === activeId && 'xl:text-primary xl:font-medium',
+                'xl:text-subtle xl:hover:text-subtle/70 transition-colors duration-300 ease-out',
+                id === activeId && 'xl:text-primary xl:hover:text-primary xl:font-medium',
                 depth === 3 && 'pl-4'
               )}
               onClick={() => scrollToTarget({ id })}
             >
               {text}
-            </li>
-          ))}
-        </ul>
-      </div>
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 };
--- src/components/typography/code/index.tsx
+++ src/components/typography/code/index.tsx
@@ -1,35 +1,20 @@
-import type { PropsWithChildren } from 'react';
-
 import { type HighlightedCode, Pre } from 'codehike/code';
 
 import collapse from '@/components/typography/code/handlers/collapse';
 import fold from '@/components/typography/code/handlers/fold';
 import mark from '@/components/typography/code/handlers/mark';
 
-interface CodeProps {
+interface Props {
   codeblock: HighlightedCode;
 }
 
-const Code = (props: CodeProps) => {
-  const meta = props.codeblock.meta;
+const Code = ({ codeblock }: Props) => {
   const handlers = [mark, fold, collapse];
 
   return (
-    <Wrapper title={meta}>
-      <Pre code={props.codeblock} handlers={handlers} />
-    </Wrapper>
-  );
-};
-
-interface WrapperProps {
-  title: string;
-}
-
-const Wrapper = ({ title, children }: PropsWithChildren<WrapperProps>) => {
-  return (
     <div className="border-line bg-background divide-line my-4 divide-y rounded-lg border text-sm leading-6">
-      {title && <div className="px-4 py-2 font-mono tracking-tight">{title}</div>}
-      <div className="overflow-auto py-4">{children}</div>
+      {codeblock.meta && <div className="px-4 py-2 font-mono tracking-tight">{codeblock.meta}</div>}
+      <Pre code={codeblock} handlers={handlers} className="overflow-auto py-4" />
     </div>
   );
 };
