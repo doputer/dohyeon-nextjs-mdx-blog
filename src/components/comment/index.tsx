66df1b7f0b06b0a60846a7129ca7fdbcd705b928
김도현 <swputer@gmail.com>
2025-06-29 20:00:12 +0900

    feat: giscus 제거

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,49 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
-import Giscus from '@giscus/react';
-
-import config from '@/configs/config.json';
-
 const Comment = () => {
-  const [theme, setTheme] = useState(global.window?.__theme || 'light');
-
-  useEffect(() => {
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'data-theme') {
-          setTheme(global.window?.__theme);
-        }
-      });
-    });
-
-    observer.observe(document.documentElement, { attributes: true });
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  return (
-    <div>
-      <Giscus
-        id="comments"
-        repo={config.comment.repo as `${string}/${string}`}
-        repoId={config.comment.repoId}
-        category={config.comment.category}
-        categoryId={config.comment.categoryId}
-        mapping="title"
-        strict="0"
-        reactionsEnabled="0"
-        emitMetadata="0"
-        inputPosition="top"
-        theme={theme}
-        lang="ko"
-        loading="lazy"
-      />
-    </div>
-  );
+  return <></>;
 };
 
 export default Comment;
