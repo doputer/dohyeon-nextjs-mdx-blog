1a54a019fc9f2ca1df1105dcf3ba9d61d44a73c3
김도현 <swputer@gmail.com>
2025-07-02 01:11:05 +0900

    feat: 간격 조정

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,7 @@ import config from '@/configs/config.json';
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center border-t border-line pt-8 text-sm text-subtle">
+    <footer className="flex justify-center border-t border-line pt-6 text-sm text-subtle md:pt-8">
       ⓒ 2025.{' '}
       <a href={config.social.github} target="_blank" aria-label="link">
         김도현
