1cfe07982c196e09be67203e9fee20af899577b3
김도현 <swputer@gmail.com>
2025-07-02 00:01:08 +0900

    feat: use-emoji 적용

---
+++ src/components/reaction/button.tsx
@@ -0,0 +1,31 @@
+import { forwardRef, type PropsWithChildren } from 'react';
+
+import useEmoji from '@/hooks/use-emoji';
+
+interface Props {
+  emoji: string;
+  onClick: () => void;
+}
+
+const Button = forwardRef<HTMLButtonElement, PropsWithChildren<Props>>((props, ref) => {
+  const { View, play, stop } = useEmoji(props.emoji);
+
+  return (
+    <button
+      ref={ref}
+      onClick={props.onClick}
+      onMouseEnter={play}
+      onMouseLeave={stop}
+      className="group flex items-center justify-start gap-2 rounded bg-surface px-2 py-1 transition-colors duration-300 ease-out select-none hover:bg-subtle/20"
+    >
+      <div className="flex size-6 items-center justify-center text-xl transition-transform duration-300 ease-out group-hover:scale-200">
+        {View}
+      </div>
+      {props.children}
+    </button>
+  );
+});
+
+Button.displayName = 'Button';
+
+export default Button;
