2203d2cf69a87f28cf1ed3b78a667947517abebb
김도현 <swputer@gmail.com>
2025-07-01 22:14:23 +0900

    feat: 마우스가 올라갔을 때만 애니메이션 재생

---
+++ src/components/list/item.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import type { PropsWithChildren } from 'react';
+
+import useEmoji from '@/hooks/use-emoji';
+
+interface Props {
+  emoji: string;
+}
+
+const Item = ({ emoji, children }: PropsWithChildren<Props>) => {
+  const { view, play, stop } = useEmoji(emoji);
+
+  return (
+    <div className="group flex items-center gap-6 md:gap-8" onMouseEnter={play} onMouseLeave={stop}>
+      <div className="flex size-full max-h-12 max-w-12 items-center justify-center text-4xl transition-transform duration-300 ease-out group-hover:scale-150 md:max-h-14 md:max-w-14 md:text-5xl">
+        {view}
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Item;
