ed89fd2c71ab4865f9323194e141787bdc2a6a05
김도현 <swputer@gmail.com>
2025-07-03 22:56:38 +0900

    feat: 글꼴 두께 수정

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -5,7 +5,7 @@ interface CounterProps {
 
 const Counter = ({ label, count }: CounterProps) => {
   return (
-    <section className="group w-fit text-2xl font-medium tracking-tight capitalize select-none sm:text-3xl">
+    <section className="group w-fit text-2xl font-semibold tracking-tight capitalize select-none sm:text-3xl">
       {label}
       {count && <sup className="inline-block align-super text-base">{count}</sup>}
     </section>
