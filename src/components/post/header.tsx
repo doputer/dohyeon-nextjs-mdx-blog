a80de42734865b5a70edd2ab25ec50a716505809
김도현 <swputer@gmail.com>
2025-07-04 23:52:07 +0900

    feat: 포스트 헤더 수정

--- src/components/post/header.tsx
+++ src/components/post/header.tsx
@@ -1,27 +1,15 @@
-import Link from 'next/link';
-
 import type { Frontmatter } from '@/lib/MDX/types';
-import { encode } from '@/utils/uri';
 
 interface HeaderProps {
   title: Frontmatter['title'];
   date: Frontmatter['date'];
-  tags: Frontmatter['tags'];
 }
 
-const Header = ({ title, date, tags }: HeaderProps) => {
+const Header = ({ title, date }: HeaderProps) => {
   return (
     <section className="space-y-2">
       <h1 className="text-2xl font-bold tracking-tight break-keep sm:text-3xl">{title}</h1>
-      <div className="flex flex-wrap items-center gap-2">
-        <time className="text-sm">{date}</time>
-        <span className="text-sm">/</span>
-        {tags.map((tag) => (
-          <Link key={tag} href={`/tags/${encode(tag)}`} className="text-sm text-link uppercase">
-            #{tag}
-          </Link>
-        ))}
-      </div>
+      <time className="text-sm">{date}</time>
     </section>
   );
 };
