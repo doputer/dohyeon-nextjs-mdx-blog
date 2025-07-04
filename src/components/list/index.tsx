24c2bf7305ef1fd01ed25d68f999985e62d15ae1
김도현 <swputer@gmail.com>
2025-07-05 03:29:04 +0900

    feat: hover 스타일

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -21,22 +21,30 @@ const List = ({ posts }: ListProps) => {
   const list = Object.entries(group).sort(([a], [b]) => Number(b) - Number(a));
 
   return (
-    <section>
+    <section className="group">
       {list.map(([year, posts]) => (
         <section key={year} className="group/section grid grid-cols-[auto_1fr] gap-4 sm:gap-8">
           <div className="relative h-fit py-3 sm:text-lg">
-            <div className="relative z-10">{year}</div>
+            <div className="relative z-10 group-hover:text-mute group-hover/section:text-main">
+              {year}
+            </div>
             <div className="pointer-events-none absolute inset-0 -mx-1.5 my-2 rounded bg-surface opacity-0 transition-opacity duration-300 group-hover/section:opacity-100 sm:-mx-2 sm:my-1" />
           </div>
-          <ul>
+          <ul className="group/ul">
             {posts.map(({ frontmatter, slug }) => (
-              <li key={slug} className="group relative">
-                <Link href={slug} className="relative z-10 flex justify-between gap-2 py-3">
+              <li
+                key={slug}
+                className="group/li relative group-hover:text-mute group-hover/section:text-main group-hover/ul:text-mute"
+              >
+                <Link
+                  href={slug}
+                  className="relative z-10 flex justify-between gap-2 py-3 group-hover/li:text-main"
+                >
                   <h2 data-emoji={frontmatter.emoji} className="break-keep sm:text-lg">
                     {frontmatter.title}
                     <span
                       data-emoji={frontmatter.emoji}
-                      className="relative after:absolute after:top-1/2 after:ml-2 after:opacity-0 after:transition-all after:duration-300 after:content-[attr(data-emoji)] group-hover:after:-translate-y-1/2 group-hover:after:opacity-100"
+                      className="relative after:absolute after:top-1/2 after:ml-2 after:opacity-0 after:transition-all after:duration-300 after:content-[attr(data-emoji)] group-hover/li:after:-translate-y-1/2 group-hover/li:after:opacity-100"
                     />
                   </h2>
                   <time dateTime={frontmatter.date} className="shrink-0 text-mute sm:text-lg">
@@ -44,7 +52,7 @@ const List = ({ posts }: ListProps) => {
                   </time>
                 </Link>
 
-                <div className="pointer-events-none absolute inset-0 -mx-1.5 my-2 rounded bg-surface opacity-0 transition-opacity duration-300 group-hover:opacity-100 sm:-mx-2 sm:my-1" />
+                <div className="pointer-events-none absolute inset-0 -mx-1.5 my-2 rounded bg-surface opacity-0 transition-opacity duration-300 group-hover/li:opacity-100 sm:-mx-2 sm:my-1" />
               </li>
             ))}
           </ul>
