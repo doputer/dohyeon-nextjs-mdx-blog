d47265b5bbe4803c0b5759580ee5f7d1ff857d1d
김도현 <swputer@gmail.com>
2025-07-05 12:45:11 +0900

    feat: eslint 9 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,89 @@
-module.exports = {
-  extends: ['next/core-web-vitals', 'next/typescript'],
-  plugins: ['import', 'prettier'],
-  rules: {
-    // Prettier
-    'prettier/prettier': 'error',
+// import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
 
-    // React
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-    'react/react-in-jsx-scope': 'off',
+// const compat = new FlatCompat({
+//   baseDirectory: import.meta.dirname,
+// });
 
-    // Import
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
-        pathGroups: [
-          {
-            pattern: '{next*,next*/**,@next*,@next*/**}',
-            group: 'external',
-            position: 'before',
+/** @type {import('eslint').Linter.Config[]} */
+export default tseslint.config(
+  /* JavaScript */
+  eslint.configs.recommended,
+
+  /* TypeScript */
+  tseslint.configs.recommended,
+
+  /* Import */
+  {
+    extends: [importPlugin.flatConfigs.recommended],
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'type',
+            'unknown',
+          ],
+          pathGroups: [
+            {
+              pattern: '{next*,next*/**,@next*,@next*/**}',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '{react*,react*/**}',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['next', 'react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
           },
-          {
-            pattern: '{react*,react*/**}',
-            group: 'external',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['next', 'react'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
+        },
+      ],
+      'sort-imports': [
+        'error',
+        {
+          allowSeparatedGroups: true,
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: ['tsconfig.json'],
+        },
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
         },
       },
-    ],
-    'sort-imports': [
-      'error',
-      {
-        allowSeparatedGroups: true,
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-      },
-    ],
+    },
   },
-};
+
+  /* Next.js */
+  // ...compat.config({ extends: ['next', 'next/core-web-vitals'] }),
+
+  //   /* Prettier */
+  {
+    extends: [prettierPlugin],
+  }
+);
