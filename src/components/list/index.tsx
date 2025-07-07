4463abe3b763e97b61976bc14783b5db2bdac7a9
김도현 <swputer@gmail.com>
2025-07-08 00:13:02 +0900

    feat: list 스타일 수정

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -27,42 +27,50 @@ const List = ({ posts }: ListProps) => {
         <section
           key={year}
           className={cn(
-            'group/section grid grid-cols-[auto_1fr] gap-4 sm:gap-8',
-            'group-hover:text-mute group-active:text-mute'
+            'group/section grid grid-cols-[auto_1fr] gap-4 hover:text-main sm:gap-8',
+            'group-hover:text-soft'
           )}
         >
           <span
             className={cn(
-              'relative h-fit py-3 transition-colors duration-300 ease-out sm:text-lg',
-              'group-hover/section:text-main group-active/section:text-main',
-              'before:absolute before:-inset-x-1 before:inset-y-2 before:-z-10 before:rounded before:bg-surface before:opacity-0 before:transition-opacity before:duration-300 group-hover/section:before:opacity-100 group-active/section:before:opacity-100 sm:before:-inset-x-2 sm:before:inset-y-1'
+              'relative h-fit py-3 transition-colors duration-300 ease-out select-none sm:text-lg',
+              'before:absolute before:-inset-x-1 before:inset-y-2 before:-z-10 before:rounded before:bg-surface before:opacity-0 before:transition-opacity before:duration-300 group-hover/section:before:opacity-100 sm:before:-inset-x-2 sm:before:inset-y-1'
             )}
           >
             {year}
           </span>
-          <ul className="group/ul">
+          <ul className={cn('group/ul', 'hover:text-soft')}>
             {posts.map(({ frontmatter, slug }) => (
               <li
                 key={slug}
                 className={cn(
                   'group/li relative',
-                  'group-hover/section:text-main group-active/section:text-main',
-                  'group-hover/ul:text-mute group-active/ul:text-mute',
-                  'before:absolute before:-inset-x-1 before:inset-y-2 before:-z-10 before:rounded before:bg-surface before:opacity-0 before:transition-opacity before:duration-300 hover:before:opacity-100 active:before:opacity-100 sm:before:-inset-x-2 sm:before:inset-y-1'
+                  'before:absolute before:-inset-x-1 before:inset-y-2 before:-z-10 before:rounded before:bg-surface before:opacity-0 before:transition-opacity before:duration-300 hover:before:opacity-100 sm:before:-inset-x-2 sm:before:inset-y-1'
                 )}
               >
-                <Link
-                  href={slug}
-                  className="flex justify-between py-3 group-hover/li:text-main group-active/li:text-main"
-                >
-                  <h2 className="pr-7 break-keep transition-colors duration-300 ease-out sm:text-lg">
+                <Link href={slug} className="flex justify-between gap-4 py-3">
+                  <h2
+                    className={cn(
+                      'break-keep transition-colors duration-300 ease-out sm:text-lg',
+                      'group-hover/li:text-main'
+                    )}
+                  >
                     {frontmatter.title}
                     <span
                       data-emoji={frontmatter.emoji}
-                      className="relative after:absolute after:top-1/2 after:ml-2 after:opacity-0 after:transition-all after:duration-300 after:content-[attr(data-emoji)] group-hover/li:after:-translate-y-1/2 group-hover/li:after:opacity-100 group-active/li:after:-translate-y-1/2 group-active/li:after:opacity-100"
+                      className="relative after:absolute after:top-1/2 after:ml-2 after:opacity-0 after:transition-all after:duration-300 after:content-[attr(data-emoji)] group-hover/li:after:-translate-y-1/2 group-hover/li:after:opacity-100"
                     />
                   </h2>
-                  <time dateTime={frontmatter.date} className="shrink-0 text-mute sm:text-lg">
+                  <time
+                    dateTime={frontmatter.date}
+                    className={cn(
+                      'shrink-0 text-mute transition-colors duration-300 ease-out sm:text-lg',
+                      'group-hover:text-soft',
+                      'group-hover/section:text-mute',
+                      'group-hover/ul:text-soft',
+                      'group-hover/li:!text-mute'
+                    )}
+                  >
                     {format(frontmatter.date, 'MM.dd')}
                   </time>
                 </Link>
