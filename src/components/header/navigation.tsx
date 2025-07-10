a6a1d0ba4a21490ebbe5d8c17e54d4838595a028
김도현 <swputer@gmail.com>
2025-07-10 22:27:48 +0900

    feat: 간격 수정

--- src/components/header/navigation.tsx
+++ src/components/header/navigation.tsx
@@ -5,7 +5,7 @@ import config from '@/configs/config.json';
 const Navigation = () => {
   return (
     <nav>
-      <Link href="/" className="text-2xl font-semibold">
+      <Link href="/" className="text-xl font-medium">
         {config.name}
       </Link>
     </nav>
