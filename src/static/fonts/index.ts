3bf39d0a70d87e9f7e78d1c0331aa44c0c8d679f
김도현 <swputer@gmail.com>
2026-07-28 13:40:55 +0900

    feat: 글꼴 교체 (Pretendard -> Suit)

--- src/static/fonts/index.ts
+++ src/static/fonts/index.ts
@@ -2,9 +2,12 @@ import localFont from 'next/font/local';
 
 const sans = localFont({
   src: [
-    { path: './Pretendard-Regular.subset.woff2', weight: '400' },
-    { path: './Pretendard-Medium.subset.woff2', weight: '500' },
-    { path: './Pretendard-Bold.subset.woff2', weight: '700' },
+    { path: './SUIT-Regular.woff2', weight: '400' },
+    { path: './SUIT-Medium.woff2', weight: '500' },
+    { path: './SUIT-Bold.woff2', weight: '700' },
+    // { path: './Pretendard-Regular.subset.woff2', weight: '400' },
+    // { path: './Pretendard-Medium.subset.woff2', weight: '500' },
+    // { path: './Pretendard-Bold.subset.woff2', weight: '700' },
   ],
   variable: '--font-sans',
 });
