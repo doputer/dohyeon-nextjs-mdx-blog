344a66b1096b34cc09056d6c9ddab62852c0c2df
김도현 <swputer@gmail.com>
2026-07-28 15:12:25 +0900

    feat: 코드 라인 강조를 잉크 틴트로 교체
    - 형광 노랑(bg-yellow/25) 대신 잉크 틴트(bg-accent/10) + 왼쪽 2px 잉크 바 적용
    - 인용 괘선·TOC 활성 바와 같은 잉크 문법으로 통일

--- src/components/typography/code/handlers/mark/index.tsx
+++ src/components/typography/code/handlers/mark/index.tsx
@@ -3,7 +3,12 @@ import { type AnnotationHandler, InnerLine } from 'codehike/code';
 const mark: AnnotationHandler = {
   name: 'mark',
   Line: (props) => <InnerLine merge={props} className="px-4" />,
-  AnnotatedLine: (props) => <InnerLine merge={props} className="bg-yellow/25 px-4" />,
+  AnnotatedLine: (props) => (
+    <InnerLine
+      merge={props}
+      className="bg-accent/10 px-4 shadow-[inset_2px_0_0_var(--color-accent)]"
+    />
+  ),
 };
 
 export default mark;
