addb463a49ea81c626e9ca70090490d5e9b9814d
김도현 <swputer@gmail.com>
2026-07-28 16:20:15 +0900

    feat: 코드 라인 강조 왼쪽 바 제거

--- src/components/typography/code/handlers/mark/index.tsx
+++ src/components/typography/code/handlers/mark/index.tsx
@@ -3,12 +3,7 @@ import { type AnnotationHandler, InnerLine } from 'codehike/code';
 const mark: AnnotationHandler = {
   name: 'mark',
   Line: (props) => <InnerLine merge={props} className="px-4" />,
-  AnnotatedLine: (props) => (
-    <InnerLine
-      merge={props}
-      className="bg-accent/10 px-4 shadow-[inset_2px_0_0_var(--color-accent)]"
-    />
-  ),
+  AnnotatedLine: (props) => <InnerLine merge={props} className="bg-accent/10 px-4" />,
 };
 
 export default mark;
