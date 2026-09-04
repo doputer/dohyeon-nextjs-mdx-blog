32ff4c72e466ce8900bf9495bc576c55f12ca2cb
김도현 <swputer@gmail.com>
2026-09-04 15:37:46 +0900

    refactor: 푸터에서 이메일 링크 제거

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,12 +4,6 @@ const Footer = () => {
   return (
     <footer className="flex items-center justify-center gap-4 text-sm text-muted">
       <a
-        href={`mailto:${config.social.email}`}
-        className="transition-colors duration-200 ease-out hover:text-main"
-      >
-        Email
-      </a>
-      <a
         href={config.social.github}
         target="_blank"
         rel="noreferrer"
