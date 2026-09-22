ae3b4167e2e3d499a05bea4172c8d3a73aa9b770
김도현 <swputer@gmail.com>
2026-09-22 12:57:38 +0900

    refactor: 컴포넌트 정리

--- src/app/(posts)/page.tsx
+++ src/app/(posts)/page.tsx
@@ -1,14 +1,20 @@
-import List from '@/components/list';
+import Group from '@/components/group';
 import config from '@/configs/config.json';
 import { getMdxs } from '@/lib/mdx';
 
 const Page = async () => {
   const posts = await getMdxs();
 
+  const writings = posts.filter((post) => post.frontmatter.category !== 'visualization');
+  const visualizations = posts.filter((post) => post.frontmatter.category === 'visualization');
+
   return (
     <>
       <h1 className="sr-only">{config.title}</h1>
-      <List posts={posts} />
+      <div className="flex flex-col gap-8">
+        <Group title="글" posts={writings} />
+        <Group title="시각화" posts={visualizations} />
+      </div>
     </>
   );
 };
