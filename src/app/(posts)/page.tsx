70c912e945cce44617e1f8f12159c828f31b6fb7
김도현 <swputer@gmail.com>
2026-09-22 14:10:14 +0900

    refactor: 컴포넌트 구조 수정

--- src/app/(posts)/page.tsx
+++ src/app/(posts)/page.tsx
@@ -9,13 +9,11 @@ const Page = async () => {
   const visualizations = posts.filter((post) => post.frontmatter.category === 'visualization');
 
   return (
-    <>
+    <div className="flex flex-col gap-8">
       <h1 className="sr-only">{config.title}</h1>
-      <div className="flex flex-col gap-8">
-        <Group title="글" posts={writings} />
-        <Group title="시각화" posts={visualizations} />
-      </div>
-    </>
+      <Group title="글" posts={writings} />
+      <Group title="시각화" posts={visualizations} />
+    </div>
   );
 };
 
